72e1ebdc6916e4e1190f1bdb54c4d2489fe761ac
Miao Wang <shankerwangmiao@gmail.com>
2023-05-09 00:38:38 +0800

    daemon: add protocol handling skel

--- src/main.c
+++ src/main.c
@@ -12,13 +12,18 @@
 #include "daemon.h"
 #include <time.h>
 #include <sys/socket.h>
+#include <protocol.h>
 
 
 static const size_t STACK_SIZE = 256*1024;
-
-static struct daemon g_daemon = {0};
-
 static const int NO_JOB_SLEEP_DELAY = 20 * 1000 * 1000;
+
+/*
+static const int MAX_IO_USEC = 300 * 1000;
+static const int MAX_NR_TASKS = 1000;
+*/
+static const int MAX_IO_USEC = 10 * 300 * 1000;
+static const int MAX_NR_TASKS = 2;
 
 static enum{
     NOEXIT = 0,
@@ -27,6 +32,8 @@ static enum{
 } g_exit_req = NOEXIT;
 
 static char *g_this_unit_name = NULL;
+static struct daemon g_daemon = {0};
+static int g_nr_tasks = 0;
 
 static int exit_req_handler(sd_event_source *s, const struct signalfd_siginfo *si, void *userdata){
     sd_event_source_disable_unref(s);
@@ -62,6 +69,63 @@ static int install_signals(void){
     return rc;
 }
 
+static int write_rate_limit_msg(__async__, struct msg_stream *stream, int kind, int code){
+    int length = sizeof(struct rate_limit_msg);
+    switch(kind){
+        case RATE_LIMIT_FAIL:
+            length += sizeof(struct rate_limit_fail_attr);
+            break;
+        case RATE_LIMIT_PROCEED:
+            length += 0;
+            break;
+        default:
+            return -EINVAL;
+    }
+    char buf[length];
+    memset(buf, 0, length);
+    struct rate_limit_msg *msg = (struct rate_limit_msg *)buf;
+    msg->length = length;
+    msg->type = kind;
+    if(kind == RATE_LIMIT_FAIL){
+        struct rate_limit_fail_attr *attr = (struct rate_limit_fail_attr *)msg->attr;
+        attr->reason = code;
+    }
+    int rc = msg_stream_write(__await__, stream, buf, length, MAX_IO_USEC);
+    return rc;
+}
+
+static int write_rate_limit_log(__async__, struct msg_stream *stream, const char *fmt, ...){
+    char *msg = NULL;
+    int str_length = 0;
+    va_list ap;
+    va_start(ap, fmt);
+    str_length = vasprintf(&msg, fmt, ap);
+    va_end(ap);
+    if(str_length < 0){
+        return -errno;
+    }
+    int length = str_length + sizeof(struct rate_limit_msg);
+    char buf[length];
+    memset(buf, 0, sizeof(struct rate_limit_msg));
+    struct rate_limit_msg *msg_header = (struct rate_limit_msg *)buf;
+    msg_header->length = length;
+    msg_header->type = RATE_LIMIT_LOG;
+    strncpy(msg_header->attr, msg, str_length);
+    free(msg);
+    int rc = msg_stream_write(__await__, stream, buf, length, MAX_IO_USEC);
+    return rc;
+}
+
+static void decrease_nr_tasks(void *dummy){
+    (void) dummy;
+    if(g_nr_tasks > 0){
+        g_nr_tasks--;
+    }else{
+        log_error("try to decrease g_nr_tasks below 0");
+        abort();
+    }
+}
+
 static void client_handler_async(__async__, void *arg){
     enum {
         INT_IO_ERR = 1,
@@ -72,6 +136,7 @@ static void client_handler_async(__async__, void *arg){
     alog_trace("handle incoming connection: %d", fd);
     int rc = 0;
     struct msg_stream *stream = NULL;
+    int client_error = 0;
     rc = init_msg_stream(&stream, g_daemon.event_loop, fd);
     if(rc < 0){
         alog_error("init_msg_stream failed: %s", strerror(-rc));
@@ -80,9 +145,20 @@ static void client_handler_async(__async__, void *arg){
     se_task_register_memory_to_free(__await__, stream, (void (*)(void *))destroy_msg_stream);
     msg_stream_reg_interrupt(__await__, stream, (void *)INT_IO_ERR);
 
+    if(g_nr_tasks >= MAX_NR_TASKS){
+        alog_warn("Too many tasks, reject new connection");
+        write_rate_limit_msg(__await__, stream, RATE_LIMIT_FAIL, RATE_LIMIT_FAIL_NORESOURCE);
+        shutdown_msg_stream(__await__, stream);
+        return;
+    }
+    g_nr_tasks++;
+    se_task_register_memory_to_free(__await__, NULL, decrease_nr_tasks);
+
     const struct ucred *cred = msg_stream_get_peer_cred(stream);
     alog_info("Our peer pid=%d, uid=%d", cred->pid, cred->uid);
 
+    char *scope_obj = NULL;
+    char *scope_name = NULL;
     if(g_this_unit_name == NULL){
         char *this_unit_name = NULL;
         rc = get_self_unit_name(__await__, g_daemon.sd_bus, &this_unit_name);
@@ -106,9 +182,12 @@ static void client_handler_async(__async__, void *arg){
         alog_trace("got our unit name %s", g_this_unit_name);
     }
 
-    char *scope_obj = NULL;
-    char *scope_name = NULL;
-    rc = start_transient_scope(__await__, g_daemon.sd_bus, cred->pid, &scope_name, &scope_obj, "(sv)(sv)", "After", "as", 1, g_this_unit_name, "BindsTo", "as", 1, g_this_unit_name);
+    rc = start_transient_scope(__await__, g_daemon.sd_bus, cred->pid, &scope_name, &scope_obj,
+        "(sv)(sv)(sv)",
+        "After", "as", 1, g_this_unit_name,
+        "BindsTo", "as", 1, g_this_unit_name,
+        "SendSIGHUP", "b", 1
+    );
     if(rc < 0){
         if(rc == -EINTR){
             goto interrupt;
@@ -143,50 +222,62 @@ static void client_handler_async(__async__, void *arg){
 
     alog_trace("cgroup_path=%s", cgroup_path);
 
-    char buf[256];
-    while(1){
-        rc = msg_stream_read(__await__, stream, buf, sizeof(buf) - 1, 3*1000*1000);
-        if(rc < 0){
-            if(rc == -EINTR){
-                goto interrupt;
-            }
-            alog_error("msg_stream_read failed: %s", strerror(-rc));
-            goto err_close_stream;
-        }else if(rc == 0){
-            alog_trace("read eof");
-            break;
+    #define excepted_msg_len (sizeof(struct rate_limit_msg) + sizeof(struct rate_limit_req_attr))
+    char _buf[excepted_msg_len];
+    rc = msg_stream_read(__await__, stream, _buf, excepted_msg_len, MAX_IO_USEC);
+    if(rc < 0){
+        if(rc == -EINTR){
+            goto interrupt;
         }
-        int len = rc;
-        buf[len++] = '\0';
-        alog_trace("read %d bytes: %s", len, buf);
-        rc = se_task_usleep(__await__, g_daemon.event_loop, 5*1000*1000);
-        if(rc < 0){
-            if(rc == -EINTR){
-                goto interrupt;
-            }
-            alog_error("se_task_usleep failed: %s", strerror(-rc));
-            goto err_close_stream;
+        if(rc == -ETIMEDOUT){
+            client_error = 1;
         }
-        alog_trace("after 5 sec delay");
-        rc = msg_stream_write(__await__, stream, buf, len, 3*1000*1000);
-        if(rc < 0){
-            if(rc == -EINTR){
-                goto interrupt;
-            }
-            alog_error("msg_stream_write failed: %s", strerror(-rc));
-            goto err_close_stream;
-        }
-        rc = se_task_usleep(__await__, g_daemon.event_loop, 5*1000*1000);
-        if(rc < 0){
-            if(rc == -EINTR){
-                goto interrupt;
-            }
-            alog_error("se_task_usleep failed: %s", strerror(-rc));
-            goto err_close_stream;
-        }
-        alog_trace("after 5 sec delay");
+        alog_error("msg_stream_read failed: %s", strerror(-rc));
+        goto err_close_stream;
+    }else if(rc == 0){
+        client_error = 1;
+        alog_trace("read eof");
+        rc = -ECONNRESET;
+        goto err_close_stream;
+    }else if((unsigned int)rc < excepted_msg_len){
+        client_error = 1;
+        alog_error("invalid message size: %d", rc);
+        rc = -EINVAL;
+        goto err_close_stream;
     }
+    struct rate_limit_msg *msg = (struct rate_limit_msg *)_buf;
+    struct rate_limit_req_attr *attr = (struct rate_limit_req_attr *)msg->attr;
+    if(msg->length < (unsigned int)excepted_msg_len){
+        client_error = 1;
+        alog_error("invalid message size in header: %d", msg->length);
+        rc = -EINVAL;
+        goto err_close_stream;
+    }
+    #undef excepted_msg_len
+    //disable interrupt from stream
+    msg_stream_reg_interrupt(__await__, stream, 0);
+    /*TODO: handle speedlimit*/
+    alog_info("will start task with ratelimit bps=%ld, pps=%ld", attr->limit.byte_rate, attr->limit.packet_rate);
+    write_rate_limit_log(__await__, stream, "Start task with ratelimit bps=%ld, pps=%ld", attr->limit.byte_rate, attr->limit.packet_rate);
+    write_rate_limit_msg(__await__, stream, RATE_LIMIT_PROCEED, 0);
+    shutdown_msg_stream(__await__, stream);
+    stream = NULL;
+    rc = pidfd_event_wait_for_exit(__await__, pidfd_event);
+    if(rc < 0){
+        if(rc == -EINTR){
+            goto interrupt;
+        }
+        alog_error("pidfd_event_wait_for_exit failed: %s", strerror(-rc));
+    }else{
+        alog_info("task exited");
+    }
+    alog_trace("will kill scope: %s", scope_name);
+    rc = sb_bus_call_unit_method(__await__, g_daemon.sd_bus, scope_obj, "Kill", NULL, "si", "all", SIGKILL);
+    return;
 err_close_stream:
+    write_rate_limit_log(__await__, stream, "%s Error: %s", client_error ? "Client" : "Internal", strerror(-rc));
+    write_rate_limit_msg(__await__, stream, RATE_LIMIT_FAIL, client_error ? RATE_LIMIT_FAIL_YOUR_ERROR : RATE_LIMIT_FAIL_INTERNAL);
+    shutdown_msg_stream(__await__, stream);
     return;
 err_close_fd:
     close(fd);
@@ -200,7 +291,8 @@ interrupt:
     }else if(reason == &global_interrupt_reasons.SYS_WILL_EXIT){
         alog_trace("    system will exit");
         if(stream){
-            msg_stream_write(__await__, stream, "Killed\n", 8, 3*1000*1000);
+            write_rate_limit_msg(__await__, stream, RATE_LIMIT_FAIL, RATE_LIMIT_FAIL_INTERNAL);
+            shutdown_msg_stream(__await__, stream);
         }
     }else if((uintptr_t)reason == INT_PROC_END){
         alog_trace("    process ended");
