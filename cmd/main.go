af17f52abccfed6b607f43856bf491bec7b48343
Miao Wang <shankerwangmiao@gmail.com>
2023-12-21 13:54:48 +0800

    declare bps & pps resource on startup

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	goflags "flag"
+	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -18,6 +22,17 @@ import (
 	"k8s.innull.com/trafficlimitd/pkg/clienthandler"
 	"k8s.innull.com/trafficlimitd/pkg/k8sclient"
 	"k8s.innull.com/trafficlimitd/pkg/server"
+	authenticationv1 "k8s.io/api/authentication/v1"
+	authenticationv1alpha1 "k8s.io/api/authentication/v1alpha1"
+	authenticationv1beta1 "k8s.io/api/authentication/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+
+	"k8s.innull.com/trafficlimitd/pkg/types"
 )
 
 var (
@@ -34,6 +49,9 @@ This application should be work with kubelet.`,
 	listenSock string
 	maxTasks   int
 	kubeconfig string
+	nodeName   string
+	bps        string
+	pps        string
 )
 
 // Execute executes the root command.
@@ -53,6 +71,9 @@ func init() {
 	mainCmd.Flags().StringVarP(&listenSock, "listen", "l", "/var/run/traffic-limitd.sock", "Listening socket")
 	mainCmd.Flags().IntVarP(&maxTasks, "max-tasks", "m", 110, "Max tasks")
 	mainCmd.Flags().StringVar(&kubeconfig, "kubeconfig", "", "Path to kubeconfig file with authorization and master location information.")
+	mainCmd.Flags().StringVar(&nodeName, "node-name", "", "This node name, must be same as kubelet's node name, can be infered from kubeconfig")
+	mainCmd.Flags().StringVar(&bps, "bps", "", "Bandwidth limit in bytes per secon")
+	mainCmd.Flags().StringVar(&pps, "pps", "", "Bandwidth limit in packets per secon")
 	mainCmd.MarkFlagRequired("kubeconfig")
 }
 
@@ -84,6 +105,45 @@ func run() {
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		klog.Fatal(err)
+	}
+
+	if nodeName == "" {
+		klog.Info("Node name not specified, try to get node name using SelfSubjectReview API")
+		name, err := getSelfNodeName(context.Background(), clientset)
+		if err != nil {
+			klog.Fatal(err)
+		}
+		nodeName = name
+		klog.Infof("Got node name: %v", nodeName)
+	} else {
+		klog.Infof("Using node name %v", nodeName)
+	}
+
+	if bps == "" && pps == "" {
+		klog.Warning("No bandwidth limit specified, will not declare available bandwidth to kubelet")
+	} else if bps != "" && pps != "" {
+		bpsVal, err := resource.ParseQuantity(bps)
+		if err != nil {
+			klog.Fatal("Invalid bandwidth limit specified:", err)
+		}
+		ppsVal, err := resource.ParseQuantity(pps)
+		if err != nil {
+			klog.Fatal("Invalid bandwidth limit specified:", err)
+		}
+		if bpsVal.Value() == 0 {
+			klog.Fatal("Invalid bandwidth limit specified: 0Bps")
+		}
+		if ppsVal.Value() == 0 {
+			klog.Fatal("Invalid bandwidth limit specified: 0pps")
+		}
+		klog.Infof("Will declare available bandwidth limit: %vBps, %vpps", bpsVal.Value(), ppsVal.Value())
+		err = declareTrafficResource(context.Background(), clientset, nodeName, bpsVal, ppsVal)
+		if err != nil {
+			klog.Fatal("Error encountered when declaring available bandwidth limit:", err)
+		}
+		klog.Info("Declared available bandwidth limit")
+	} else {
+		klog.Fatal("Invalid bandwidth limit specified: either bps or pps is not specified")
 	}
 
 	fetcher := k8sclient.NewK8sTrafficLimitInfoFetcher(clientset)
@@ -118,4 +178,82 @@ func run() {
 		klog.Fatal(err)
 	}
 	klog.Info("Server stopped, exiting...")
+}
+
+func getSelfNodeName(ctx context.Context, clientset *kubernetes.Clientset) (string, error) {
+	var (
+		res runtime.Object
+		err error
+	)
+	res, err = clientset.AuthenticationV1().SelfSubjectReviews().Create(ctx, &authenticationv1.SelfSubjectReview{}, metav1.CreateOptions{})
+	if err != nil && k8serrors.IsNotFound(err) {
+		res, err = clientset.AuthenticationV1beta1().SelfSubjectReviews().Create(ctx, &authenticationv1beta1.SelfSubjectReview{}, metav1.CreateOptions{})
+		if err != nil && k8serrors.IsNotFound(err) {
+			res, err = clientset.AuthenticationV1alpha1().SelfSubjectReviews().Create(ctx, &authenticationv1alpha1.SelfSubjectReview{}, metav1.CreateOptions{})
+		}
+	}
+	if err != nil {
+		if k8serrors.IsForbidden(err) {
+			return "", fmt.Errorf("Unable to detect node name: Forbidden to create SelfSubjectReview, please check RBAC")
+		} else if k8serrors.IsNotFound(err) {
+			return "", fmt.Errorf("Unable to detect node name: No SelfSubjectReview API found")
+		} else {
+			return "", fmt.Errorf("Unable to detect node name: %v", err)
+		}
+	}
+	var username string
+	switch res.(type) {
+	case *authenticationv1.SelfSubjectReview:
+		username = res.(*authenticationv1.SelfSubjectReview).Status.UserInfo.Username
+	case *authenticationv1beta1.SelfSubjectReview:
+		username = res.(*authenticationv1beta1.SelfSubjectReview).Status.UserInfo.Username
+	case *authenticationv1alpha1.SelfSubjectReview:
+		username = res.(*authenticationv1alpha1.SelfSubjectReview).Status.UserInfo.Username
+	default:
+		return "", fmt.Errorf("Unable to detect node name: SelfSubjectReview returned unexpected response type %T", res)
+	}
+	if username == "" {
+		return "", fmt.Errorf("Unable to detect node name: SelfSubjectReview returned empty username")
+	}
+	if strings.HasPrefix(username, "system:node:") {
+		return strings.TrimPrefix(username, "system:node:"), nil
+	} else {
+		return "", fmt.Errorf("Unable to detect node name: SelfSubjectReview returned unexpected username %s, expected name with prefix system:node:", username)
+	}
+}
+
+func declareTrafficResource(ctx context.Context, clientset *kubernetes.Clientset, nodeName string, bps, pps resource.Quantity) error {
+
+	var patch []map[string]interface{}
+	rfc6901Encoder := strings.NewReplacer("~", "~0", "/", "~1")
+	patch = append(patch, map[string]interface{}{
+		"op":    "add",
+		"path":  "/status/capacity/" + rfc6901Encoder.Replace(types.BytesPerSecondResourceName),
+		"value": bps.String(),
+	})
+	patch = append(patch, map[string]interface{}{
+		"op":    "add",
+		"path":  "/status/capacity/" + rfc6901Encoder.Replace(types.PacketsPerSecondResourceName),
+		"value": pps.String(),
+	})
+
+	patchBytes, err := json.Marshal(patch)
+
+	if err != nil {
+		return err
+	}
+
+	err = clientset.CoreV1().RESTClient().Patch(k8stypes.JSONPatchType).
+		Resource("nodes").
+		Name(nodeName).
+		SubResource("status").
+		Body(patchBytes).
+		Do(ctx).
+		Into(&corev1.Node{})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -8,9 +8,10 @@ require (
 	github.com/opencontainers/runtime-spec v1.1.0-rc.1
 	github.com/spf13/cobra v1.7.0
 	github.com/vishvananda/netlink v1.2.1-beta.2
-	golang.org/x/sys v0.9.0
-	k8s.io/apimachinery v0.26.2
-	k8s.io/client-go v0.26.2
+	golang.org/x/sys v0.13.0
+	k8s.io/api v0.29.0
+	k8s.io/apimachinery v0.29.0
+	k8s.io/client-go v0.29.0
 )
 
 require (
@@ -30,26 +31,26 @@ require (
 	github.com/cyphar/filepath-securejoin v0.2.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
-	github.com/emicklei/go-restful/v3 v3.10.1 // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/jsonreference v0.20.0 // indirect
-	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/gnostic v0.5.7-v3refs // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
-	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/moby/locker v1.0.1 // indirect
 	github.com/moby/sys/mountinfo v0.6.2 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
@@ -70,32 +71,31 @@ require (
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/otel v1.14.0 // indirect
 	go.opentelemetry.io/otel/trace v1.14.0 // indirect
-	golang.org/x/mod v0.9.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.4.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
-	golang.org/x/tools v0.7.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
-	google.golang.org/protobuf v1.29.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.26.2 // indirect
 	k8s.io/cri-api v0.27.1 // indirect
-	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
-	k8s.io/utils v0.0.0-20230220204549-a5ecb0141aa5 // indirect
-	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	k8s.io/klog/v2 v2.100.1
+	k8s.io/klog/v2 v2.110.1
 )
